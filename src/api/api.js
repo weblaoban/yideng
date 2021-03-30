b47f7e193f3350d93777a4004a7dc0c53267f059
weblaoban <904726194@qq.com>
2021-03-30 17:07:33 +0800

    jiekou

--- src/api/api.js
+++ src/api/api.js
@@ -4,6 +4,7 @@ const url = {
     logout: 'customer/logout', //推出登录接口
     modifyPas: 'customer/updatePwd', //修改密码
     list: 'freightOrder/list', //列表
+    detail: 'freightOrder/detail', //详情   参数freightOrderId
 }
 for (let key in url) {
     url[key] = host + url[key]
