ce4f5315f0707d9ef4fd53a2ebf7eed1a8183343
EDZ <edz@EDZdeMacBook-Pro.local>
2021-04-01 23:23:13 +0800

    服务中英文

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -7,7 +7,7 @@
       <div class="service">
         <div class="title">
           <div class="circle"></div>
-          <p>国际空运</p>
+          <p v-text="$t('lang.serviceTitle')"></p>
           <div class="line"></div>
         </div>
         <div class="content clear">
@@ -15,13 +15,13 @@
             <img src="../assets/image/service/service1.png" alt />
           </div>
           <div class="left">
-            <p>上海伊登已经在海外主要国家建立了完善的国际合作网络，为您提供：</p>
+            <p v-text="$t('lang.servicedes')"></p>
             <ul>
               <li>
-                <p>空运进出口</p>
+                <p v-text="$t('lang.servicedes2')"></p>
               </li>
               <li>
-                <p>提货-仓储-订舱-清关-保险-送货等全方位服务</p>
+                <p v-text="$t('lang.servicedes3')"></p>
               </li>
             </ul>
             <p style="margin-bottom: 0px;">
@@ -35,9 +35,10 @@
               ，每一环都值得信赖，全程为您的货物保驾护航。
             </p>
             <p style="margin-bottom: 0px;">
-              <span>经济 快捷</span>
+              <span v-text="$t('lang.servicedes12')"></span>
             </p>
             <p>
+              {{ $t('lang.servicedes13')}}
               上海伊登依托丰富的航线资源，以经济、快捷为服务宗旨，
               <br />根据您的需求，为您包板、包舱、包机
               <br />以优廉的价格和优质的服务
@@ -49,7 +50,7 @@
       <div class="service">
         <div class="title">
           <div class="circle"></div>
-          <p>国际海运</p>
+          <p>{{$t('lang.serviceTitle2')}}</p>
           <div class="line"></div>
         </div>
         <div class="content clear">
@@ -59,9 +60,9 @@
 
           <div class="right">
             <p>
-              上海伊登国际拥有交通部批准的无船承运人(NVOCC)经营资格。
-              <br />主要经营各类货物的海运进出口运输
-              <br />十几年来，我们积累了丰富的运力资源和实践经验，不断完善服务网络的覆盖范围，现已与COSCO, MAERSK, MSC, EVERGREEN, YANGMING, WANHAI等多家船公司保持了长久稳定的合作关系。
+              {{$t('lang.service2des')}}
+              <br />{{$t('lang.service2des2')}}
+              <br />{{$t('lang.service2des3')}}
               <br />上海伊登立志为客户提供
               <span>更经济、更可靠、更高效</span>的一站式物流服务，船期稳定，运价优惠，是我公司赢得客户青睐的优势之一。
             </p>
@@ -71,7 +72,7 @@
       <div class="service">
         <div class="title">
           <div class="circle"></div>
-          <p>跨境铁路运输</p>
+          <p>{{$t('lang.serviceTitle3')}}</p>
           <div class="line"></div>
         </div>
         <div class="content clear">
@@ -79,15 +80,15 @@
             <img src="../assets/image/service/service3.png" alt />
           </div>
           <div class="left">
-            <p>上海伊登为您提供中欧、中亚、中蒙等跨境铁路运输服务，包括班列订舱服务、报关服务、提货和送货服务、整箱和拼箱服务。</p>
-            <p>我们与国内外数十个铁路口岸保持良好的合作往来，主要包括西安、郑州、成都、重庆、沈阳、武汉、汉堡、华沙、马拉、杜伊斯堡、乌兰巴托、中亚各国铁路口岸等。</p>
+            <p>{{$t('lang.service3des')}}</p>
+            <p>{{$t('lang.service3des2')}}</p>
           </div>
         </div>
       </div>
       <div class="service">
         <div class="title">
           <div class="circle"></div>
-          <p>合同物流</p>
+          <p>{{$t('lang.serviceTitle4')}}</p>
           <div class="line"></div>
         </div>
         <div class="content clear">
@@ -95,8 +96,8 @@
             <img src="../assets/image/service/service4.png" alt />
           </div>
           <div class="right">
-            <p>上海伊登国际为客户提供多种合同物流方案，包括国内运输和国内仓储，非保税和保税业务，危险品，冷藏冷冻品以及各类大件项目运输服务。</p>
-            <p>我们在全国搭建了多网络平台，在上海拥有上万平米的转运中心及物流仓储中心。自有及合同车辆千余部，门到门运输服务覆盖全国1,000多个城市。</p>
+            <p>{{$t('lang.service4des')}}</p>
+            <p>{{$t('lang.service4des2')}}</p>
           </div>
         </div>
       </div>
